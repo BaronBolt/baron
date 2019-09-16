a012d430bbe7d4a79b0d725077df1062b1132fb4
BaronBolt <55134703+BaronBolt@users.noreply.github.com>
2019-09-16 07:32:51 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+
+function Donate (){
+		alert("Bank Name. sterling plc. Acct. Name: ELIJAH OLASUKANMI AREMU Acct. No:.0071439652");
+		
+}
+
+function Contact(){
+		alert("Phone No:. 08174213617, Email: aremueliboy@gmail.com");
+		
+}
